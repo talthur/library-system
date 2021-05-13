182a3d8a0ffd83fd397c3112f0dec488b673bed4
talthur <talthur.ga@gmail.com>
2021-05-13 19:28:59 -0300

    Inserting new endpoints for the security

--- src/main/java/br/com/talthur/librarysystem/controller/LoginController.java
+++ src/main/java/br/com/talthur/librarysystem/controller/LoginController.java
@@ -1,13 +1,30 @@
 package br.com.talthur.librarysystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/")
+	@GetMapping("/login")
 	public String login() {
 		return "/login";
+	}
+	
+	@PostMapping("/login")
+	public String home() {
+		return "redirect:/home";
+	}
+//	
+	@RequestMapping("/")
+	public String start() {
+		return "redirect:/home";
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return "/admin";
 	}
 }
--- src/main/java/br/com/talthur/librarysystem/security/SecurityConfiguration.java
+++ src/main/java/br/com/talthur/librarysystem/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -20,12 +21,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+		http.authorizeRequests().		
+		antMatchers("/").access("hasAnyAuthority('USER','ADMIN')").
+		antMatchers("/home").access("hasAnyAuthority('USER','ADMIN')").
+		antMatchers("/admin").access("hasAuthority('ADMIN')").
+		anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).authorities("USER");
+		auth.inMemoryAuthentication()
+		.withUser("Arthur").password(passwordEncoder().encode("1234")).authorities("ADMIN").and()
+		.withUser("user").password(passwordEncoder().encode("1234")).authorities("USER");
+		
 	}
 	
 	
